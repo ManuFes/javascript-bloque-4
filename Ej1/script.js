7d25cff35ff686b73b94564091a4f76bc7e15e5e
Manuel <mfespal560@g.educaand.es>
2024-06-03 18:35:07 +0200

    asopla

--- Ej1/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('showMessage').onclick = function() {
-    console.log("Hola mundo");
-    alert("Hola mundo");
-    document.getElementById('message').innerHTML = "Hola mundo";
-}
